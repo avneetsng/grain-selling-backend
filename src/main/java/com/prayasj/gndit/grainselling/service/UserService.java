e18c3ff39ce38d7fe3e4902f486b913646e8b229
Prateek Jain <prateek.jain23995@yahoo.com>
2018-02-17 22:21:03 +0530

    encoding password when saving in database

--- src/main/java/com/prayasj/gndit/grainselling/service/UserService.java
+++ src/main/java/com/prayasj/gndit/grainselling/service/UserService.java
@@ -4,6 +4,7 @@ import com.prayasj.gndit.grainselling.dto.UserInfo;
 import com.prayasj.gndit.grainselling.model.User;
 import com.prayasj.gndit.grainselling.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +13,15 @@ public class UserService {
   @Autowired
   private UserRepository userRepository;
 
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
   public boolean signup(UserInfo userInfo) {
     if (userRepository.findByUsername(userInfo.getUsername()) != null) {
       return false;
     }
-    userRepository.save(new User(userInfo.getUsername(), userInfo.getPassword()));
+    String encodedPassword = passwordEncoder.encode(userInfo.getPassword());
+    userRepository.save(new User(userInfo.getUsername(), encodedPassword));
     return true;
   }
 }
